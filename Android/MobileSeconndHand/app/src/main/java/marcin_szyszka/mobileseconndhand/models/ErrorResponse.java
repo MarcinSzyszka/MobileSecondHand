d76c51109e27ca9764a8565ed3196fea39fba481
Ćwiara <szyszka_marcin@wp.pl>
2016-03-02 21:21:52 +0100

    Working with authentication - added sign, register, end error response models in Android Project

---
+++ Android/MobileSeconndHand/app/src/main/java/marcin_szyszka/mobileseconndhand/models/ErrorResponse.java
@@ -0,0 +1,12 @@
+package marcin_szyszka.mobileseconndhand.models;
+
+/**
+ * Created by marcianno on 2016-03-02.
+ */
+public class ErrorResponse {
+    public String ErrorMessage;
+
+    public ErrorResponse(){
+
+    }
+}
---
+++ Android/MobileSeconndHand/app/src/main/java/marcin_szyszka/mobileseconndhand/models/LoginModel.java
@@ -0,0 +1,13 @@
+package marcin_szyszka.mobileseconndhand.models;
+
+/**
+ * Created by marcianno on 2016-03-02.
+ */
+public class LoginModel {
+    public String Email;
+    public String Password;
+
+    public LoginModel(){
+
+    }
+}
