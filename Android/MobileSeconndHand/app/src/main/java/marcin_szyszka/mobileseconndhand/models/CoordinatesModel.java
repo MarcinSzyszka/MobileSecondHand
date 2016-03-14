50159cdc13f1e708b78dbeeb37b585c3af55efa6
Ćwiara <szyszka_marcin@wp.pl>
2016-03-14 20:51:21 +0100

    Android - model with coordinates to send with request for advertisements

---
+++ Android/MobileSeconndHand/app/src/main/java/marcin_szyszka/mobileseconndhand/models/CoordinatesModel.java
@@ -0,0 +1,14 @@
+package marcin_szyszka.mobileseconndhand.models;
+
+/**
+ * Created by marcianno on 2016-03-14.
+ */
+public class CoordinatesModel {
+    public double Latitude;
+    public double Longitude;
+    public int MaxDistance;
+
+    public CoordinatesModel(){
+
+    }
+}
