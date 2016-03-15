4343bd4226aaa29ce7c9866ad2bcc80e04c87f6a
Ćwiara <szyszka_marcin@wp.pl>
2016-03-15 22:00:14 +0100

    Asp.Net - add common model and service to calculatind coordinates

---
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand.Common/CoordinatesHelpers/ICoordinatesCalculator.cs
@@ -0,0 +1,8 @@
+﻿using MobileSecondHand.Common.Models;
+
+namespace MobileSecondHand.Common.CoordinatesHelpers {
+	public interface ICoordinatesCalculator {
+		CoordinatesForSearchingAdvertisementsModel GetCoordinatesForSearchingAdvertisements(double lat, double lon, int distanceInKm);
+		double GetDistanceBetweenTwoLocalizations(double latitude1, double longitude1, double latitude2, double longitude2);
+	}
+}
---
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand.Common/Models/CoordinatesForSearchAdvertisementsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileSecondHand.Common.Models
+{
+    public class CoordinatesForSearchingAdvertisementsModel
+    {
+		public double LatitudeStart { get; set; }
+		public double LatitudeEnd { get; set; }
+		public double LongitudeStart { get; set; }
+		public double LongitudeEnd { get; set; }
+	}
+}
