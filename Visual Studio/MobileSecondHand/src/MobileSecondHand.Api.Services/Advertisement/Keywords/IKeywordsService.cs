be61e30ff6ba513cd0c04a97a0804b417f64af3f
Ćwiara <szyszka_marcin@wp.pl>
2016-04-18 20:38:22 +0200

    Asp.Net - api service for recognizing keywords from text, getting from db or creating new keywords

---
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand.Api.Services/Advertisement/Keywords/IKeywordsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MobileSecondHand.Db.Models.Advertisement.Keywords;
+
+namespace MobileSecondHand.Api.Services.Advertisement.Keywords
+{
+    public interface IKeywordsService
+    {
+		ICollection<T> RecognizeAndGetKeywords<T>(string textToRecognize) where T : IKeyword;
+    }
+}
