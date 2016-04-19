f489628fcaa2fd6b6eaa53ea82622f3d33a90388
Ćwiara <szyszka_marcin@wp.pl>
2016-04-19 19:53:25 +0200

    Asp.Net - Updated keywords db models

--- Visual Studio/MobileSecondHand/src/MobileSecondHand.Db.Models/Advertisement/Keywords/ColorKeyword.cs
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand.Db.Models/Advertisement/Keywords/ColorKeyword.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace MobileSecondHand.Db.Models.Advertisement.Keywords {
-	public class ColorKeyword : IKeyword {
+	public class ColorKeyword : IKeywordDbModel {
 		public int Id { get; set; }
 		public string Name { get; set; }
 
