53c7983ee46adad22035f483a49e4f3229b525c5
Ćwiara <szyszka_marcin@wp.pl>
2016-04-19 19:46:11 +0200

    Asp.Net - update keyword samples

--- Visual Studio/MobileSecondHand/src/MobileSecondHand.Common/Keywords/KeywordsHelper.cs
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand.Common/Keywords/KeywordsHelper.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MobileSecondHand.Common.Keywords
-{
-    public class KeywordsHelper
-    {
+namespace MobileSecondHand.Common.Keywords {
+	public class KeywordsHelper {
 		public IDictionary<string, List<string>> CategoryKeywords { get; private set; }
 		public IDictionary<string, List<string>> ColorKeywords { get; private set; }
 
@@ -103,8 +101,12 @@ namespace MobileSecondHand.Common.Keywords
 			this.CategoryKeywords.Add("Bluz", new List<string> {
 				"Bluzki",
 				"Bluzeczka",
+				"Bluzeczki",
+				"Bluzeczkę",
 				"Bluzka",
+				"Bluzkę",
 				"Bluza",
+				"Bluzę",
 				"Bluzy"
 			});
 			this.CategoryKeywords.Add("Body", new List<string> {
@@ -113,26 +115,33 @@ namespace MobileSecondHand.Common.Keywords
 			this.CategoryKeywords.Add("Boler", new List<string> {
 				"Bolerka",
 				"Bolerko",
-				"Bolero"
+				"Bolero",
+				"Bolerka"
 			});
 			this.CategoryKeywords.Add("Dres", new List<string> {
 				"Dresy",
 				"Dress",
 				"Dres",
+				"Dresik",
+				"Dresiki",
 				"Dresowa",
 				"Dresowy",
 				"Dresowe"
 			});
 			this.CategoryKeywords.Add("Garson", new List<string> {
 				"Garsonki",
+				"Garsonkę",
 				"Garsonka",
-				"Garsoneczka"
+				"Garsoneczka",
+				"Garsoneczkę",
+				"Garsoneczki"
 			});
 			this.CategoryKeywords.Add("Golf", new List<string> {
 				"Golfy",
 				"Golf",
 				"Golfik",
-				"Golfem"
+				"Golfem",
+				"Golfa"
 			});
 			this.CategoryKeywords.Add("Gorset", new List<string> {
 				"Gorsety",
@@ -142,44 +151,56 @@ namespace MobileSecondHand.Common.Keywords
 			this.CategoryKeywords.Add("Kamizel", new List<string> {
 				"Kamizelki",
 				"Kamizelka",
-				"Kamizeleczka"
+				"Kamizeleczka",
+				"Kamizelkę",
+				"Kamizelką"
 			});
 			this.CategoryKeywords.Add("Kombinezon", new List<string> {
 				"Kombinezony",
 				"Kombinezon",
 				"Kombinezonik",
-				"Kombinezonu"
+				"Kombinezonu",
+				"Kombinezonem"
 			});
 			this.CategoryKeywords.Add("Kostium", new List<string> {
 				"Kostiumy",
-				"Kostium"
+				"Kostium",
+				"Kostiumu",
+				"Kostiumik"
 			});
 			this.CategoryKeywords.Add("Koszulk", new List<string> {
 				"Koszulki",
 				"Koszulka",
-				"Koszulkę",
-				"T-shirt",
-				"Tshirt"
+				"Koszulkę"
+				//"T-shirt",
+				//"T-shirtu",
+				//"Tshirt",
+				//"Tshirtu"
 			});
 			this.CategoryKeywords.Add("Koszul", new List<string> {
 				"Koszule",
 				"Koszulę",
-				"Koszula"
+				"Koszula",
+				"Koszuli"
 			});
 			this.CategoryKeywords.Add("Leg", new List<string> {
 				"Legginsy",
-				"Leginsy"
+				"Leginsy",
+				"Leginsami",
+				"Legginsami"
 			});
 			this.CategoryKeywords.Add("Marynar", new List<string> {
 				"Marynarki",
 				"Marynarka",
 				"Marynareczka",
 				"Marynara",
+				"Marynarkę"
 			});
 			this.CategoryKeywords.Add("Spod", new List<string> {
 				"Spodnie",
 				"Spodenki",
-				"Spodeneczki"
+				"Spodeneczki",
+				"Spodni",
 			});
 			this.CategoryKeywords.Add("Spódni", new List<string> {
 				"Spódnice/Spódniczki",
@@ -192,20 +213,24 @@ namespace MobileSecondHand.Common.Keywords
 			});
 			this.CategoryKeywords.Add("Suk", new List<string> {
 				"Sukienki",
+				"Sukienka",
 				"Sukienkę",
 				"Sukieneczkę",
-				"Suknię",
-				"Kiecka",
-				"Kieca",
-				"Kieckę",
-				"Kiecę",
+				"Suknię"
+				//"Kiecka",
+				//"Kieca",
+				//"Kieckę",
+				//"Kiecę",
+				//"Kiecki"
 			});
 			this.CategoryKeywords.Add("Swet", new List<string> {
 				"Swetry",
 				"Swetr",
 				"Sweter",
 				"Sweterek",
-				"Sweterkiem"
+				"Sweterkiem",
+				"Swetra",
+				"Sweterki"
 			});
 			this.CategoryKeywords.Add("Tuni", new List<string> {
 				"Tuniki",
@@ -216,7 +241,16 @@ namespace MobileSecondHand.Common.Keywords
 			});
 			this.CategoryKeywords.Add("But", new List<string> {
 				"Buty",
-				"Butki"
+				"Butki",
+				"Butów"
+				//"Sandały",
+				//"Szpilki",
+				//"Obcasy",
+				//"Baleriny",
+				//"Balerinki",
+				//"Kozaki",
+				//"Klapki",
+				//"Japonki"
 			});
 		}
 	}
