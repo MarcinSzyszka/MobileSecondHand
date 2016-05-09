5439ab41c60048dce4aec08d252f8ac7cedbe3e6
Ćwiara <szyszka_marcin@wp.pl>
2016-05-09 20:00:58 +0200

    Asp.Net - Create default ChutHub

---
+++ Visual Studio/MobileSecondHand/src/MobileSecondHand/Hubs/MobileSecondHandChatHub.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * In the Application_Start method, or somewhere in app's start
+ * you'll need to add the namespace inclusion:
+ * 
+ * using Microsoft.AspNet.SignalR;
+ * 
+ * and the code:
+ * 
+ * RouteTable.Routes.MapHubs();
+ * 
+ * */
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MobileSecondHand.Hubs
+{
+	[Authorize]
+	[HubName("MobileSecondHandChatHub")]
+    public class MobileSecondHandChatHub : Hub
+    {
+
+        public void Register(string message)
+        {
+			var user = Context.User;
+            base.Clients.All.UpdateChatMessage("Zarejestrowane usera : " + user.Identity.IsAuthenticated + " " + message);
+        }
+    }
+}
