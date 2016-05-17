3a7fc688bc79b33cfab3beafb81f7e74a5dc1c9f
Ćwiara <szyszka_marcin@wp.pl>
2016-05-17 20:12:33 +0200

    Xamarin - chat message model

---
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.Models/Chat/ConversationMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileSecondHand.Models.Chat {
+	public class ConversationMessage {
+		public string SenderId { get; set; }
+		public string MessageHeader { get; set; }
+		public string MessageContent { get; set; }
+		public bool UserWasSender { get; set; }
+	}
+}
