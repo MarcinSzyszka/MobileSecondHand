70b60ebf983dfee385a6555be941f23e71481615
Ćwiara <szyszka_marcin@wp.pl>
2016-05-26 12:00:10 +0200

    Deleted old porject Asp.Net Core Rc1

--- Visual Studio/MobileSecondHand/src/MobileSecondHand.Api.Services/CacheServices/IChatHubCacheService.cs
+++
@@ -1,8 +0,0 @@
-﻿using MobileSecondHand.Api.Models.ChatHub;
-
-namespace MobileSecondHand.Api.Services.CacheServices {
-	public interface IChatHubCacheService {
-		void AddConnectedClient(UserConnection userConnection);
-		void RemoveDisconnectedClient(string connectionId);
-	}
-}
--- Visual Studio/MobileSecondHand/src/MobileSecondHand.Api.Services/OutsideApisManagers/IFacebookApiManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-using MobileSecondHand.Api.Models.OutsideApisModels;
-
-namespace MobileSecondHand.Api.Services.OutsideApisManagers {
-	public interface IFacebookApiManager {
-		Task<FacebookUserCredentialsResponse> GetUserCredentials(string facebookToken);
-	}
-}
