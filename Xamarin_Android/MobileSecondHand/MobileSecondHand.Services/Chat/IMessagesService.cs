43888e2fdf903f5d0432d671dac62a0147f0d57c
Ćwiara <szyszka_marcin@wp.pl>
2016-06-23 10:18:53 +0200

    Xamarin - newr serwice for messages

---
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.Services/Chat/IMessagesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobileSecondHand.Models.Chat;
+
+namespace MobileSecondHand.Services.Chat {
+	public interface IMessagesService {
+		Task<List<ConversationMessage>> GetMessages(int conversationId, int pageNumber, string bearerToken);
+	}
+}
---
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.Services/Chat/MessagesService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MobileSecondHand.Models.Chat;
+using MobileSecondHand.Models.Consts;
+using MobileSecondHand.Models.Security;
+using Newtonsoft.Json;
+
+namespace MobileSecondHand.Services.Chat {
+	public class MessagesService : IMessagesService {
+
+		public async Task<List<ConversationMessage>> GetMessages(int conversationId, int pageNumber, string bearerToken) {
+			List<ConversationMessage> messagesList = new List<ConversationMessage>();
+			var client = new HttpClient();
+			client.BaseAddress = new Uri(WebApiConsts.WEB_API_URL);
+			client.DefaultRequestHeaders.Add(WebApiConsts.AUTHORIZATION_HEADER_NAME, WebApiConsts.AUTHORIZATION_HEADER_BEARER_VALUE_NAME + bearerToken);
+
+			var response = await client.GetAsync(String.Format("{0}/{1}/{2}", WebApiConsts.CONVERSATION_CONTROLLER, "GetMessages", conversationId, pageNumber));
+			
+			if (response.IsSuccessStatusCode) {
+				var responseString = await response.Content.ReadAsStringAsync();
+				messagesList = JsonConvert.DeserializeObject<List<ConversationMessage>>(responseString);
+			}
+
+			return messagesList;
+		}
+	}
+}
