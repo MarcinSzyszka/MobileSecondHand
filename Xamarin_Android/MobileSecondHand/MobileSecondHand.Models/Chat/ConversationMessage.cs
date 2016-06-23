58d0f4fa0227a7c6b121cc34c93443f03576d8f3
Ćwiara <szyszka_marcin@wp.pl>
2016-06-23 10:19:18 +0200

    Xamarin - updated model

--- Xamarin_Android/MobileSecondHand/MobileSecondHand.Models/Chat/ConversationMessage.cs
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.Models/Chat/ConversationMessage.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace MobileSecondHand.Models.Chat {
 	public class ConversationMessage {
-		public string SenderId { get; set; }
+		public string Id { get; set; }
 		public string MessageHeader { get; set; }
 		public string MessageContent { get; set; }
 		public bool UserWasSender { get; set; }
+		public int ConversationId { get; set; }
 	}
 }
