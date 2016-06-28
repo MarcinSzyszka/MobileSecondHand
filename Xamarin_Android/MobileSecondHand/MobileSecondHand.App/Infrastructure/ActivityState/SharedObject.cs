71c0ecf1796ad43fea5ab99d6fb9b3cde6d07705
Ćwiara <szyszka_marcin@wp.pl>
2016-06-28 18:10:35 +0200

    Xamarin - update components

---
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.App/Infrastructure/ActivityState/SharedObject.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+
+namespace MobileSecondHand.App.Infrastructure.ActivityState
+{
+	public class SharedObject
+	{
+		public static object Data { get; set; }
+	}
+
+}
---
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileSecondHand.Common.Extensions
+{
+	public static class StringExtensions
+	{
+		public static bool IsNotNullOrEmpty(this string stringValue)
+		{
+			return stringValue != null && stringValue != String.Empty;
+		}
+	}
+}
