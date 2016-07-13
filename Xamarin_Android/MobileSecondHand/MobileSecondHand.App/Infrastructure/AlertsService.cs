4163b3777bfd13f4e6a336c681485d04d1cdaf58
Ćwiara <szyszka_marcin@wp.pl>
2016-07-13 19:36:25 +0200

    Xamarin - dorobienie confoirma do serwisu z alertami

--- Xamarin_Android/MobileSecondHand/MobileSecondHand.App/Infrastructure/AlertsService.cs
+++ Xamarin_Android/MobileSecondHand/MobileSecondHand.App/Infrastructure/AlertsService.cs
@@ -10,16 +10,36 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
-namespace MobileSecondHand.App.Infrastructure {
-	public class AlertsService {
-		public static void ShowToast(Context context, string message) {
+namespace MobileSecondHand.App.Infrastructure
+{
+	public class AlertsService
+	{
+		public static void ShowToast(Context context, string message)
+		{
 			Toast.MakeText(context, message, ToastLength.Long).Show();
 		}
 
-		public static void ShowAlertDialog(Context context, string message) {
+		public static void ShowAlertDialog(Context context, string message)
+		{
 			AlertDialog.Builder alert = new AlertDialog.Builder(context);
 			alert.SetTitle("Informacja");
 			alert.SetMessage(message);
+			alert.Show();
+		}
+
+		public static void ShowConfirmDialog(Context context, string message, Action actionOnConfirm, Action actionOnCancel)
+		{
+			AlertDialog.Builder alert = new AlertDialog.Builder(context);
+			alert.SetTitle("PotwierdŸ");
+			alert.SetMessage(message);
+			alert.SetPositiveButton("Tak", (sender, args) => {
+				actionOnConfirm();
+				alert.Dispose();
+			});
+			alert.SetNegativeButton("Nie", (sender, args) => {
+				actionOnCancel();
+				alert.Dispose();
+			});
 			alert.Show();
 		}
 	}
